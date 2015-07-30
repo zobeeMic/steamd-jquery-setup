53290fb5eba827fa19d0f45a59ff95615d05ce01
LCAir4 <lcair4@172-17-2-143.DYNAPOOL.NYU.EDU>
2015-07-30 10:27:20 -0400

    changed stuff

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-    $("div").click(function(){
+    $("p").click(function(){
         $(this).hide();
     });
+});
+
+$('#alert-button').click(function(){
+	alert('Hello World!');
+});
+
+
+
+$('#bt2').click(function(){
+	$('#hide-p').hide();
+});
+
+
+$('#showbtn').click(function(){
+	$('#hide-p').show();
 });
